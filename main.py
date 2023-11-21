f70af1d183cbc82fd244dfc053376a16bcf7c897
GeekyLiam <liamhefford7@gmail.com>
2023-11-21 21:21:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import win32gui
 import time
 
-def heal_pokemon:
-
+def heal_pokemon():
+    pass
 
 def main():
-    pass
+    window = win32gui.GetForegroundWindow()
+    print(window)
 
 if __name__ == "__main__":
     main()
