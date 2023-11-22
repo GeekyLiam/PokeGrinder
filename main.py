2c5dc67054ebf18654d36e14f989ca2ac4863a6c
GeekyLiam <liamhefford7@gmail.com>
2023-11-22 22:27:26 +0000

    Added functionality

--- main.py
+++ main.py
@@ -1,9 +1,72 @@
 import ctypes
 import time
+import win32gui
+import keyboard
+import getpixelcolor
+
+from modules import frlg
+from modules import ingame_controls as igc
+
+def menu_select(menu_option):
+    """
+    Opens menu in game, and selects given menu option by checking pixel colors.
+    Param: menu_option: Which menu option you want to select.
+    """
+
+    #TODO: make function adaptable to window position changes by +/- from x/y pos - make window pos modifiable within an ini file.
+
+def battle():
+    pass
+
+def battle_check():
+    """
+    Checks if game is in battle state by checking pixel colors.
+    Runs battle function if in battle, returns False if not in battle.
+    """
+
+    #TODO: make function adaptable to window position changes by +/- from x/y pos - make window pos modifiable within an ini file.
+
+    pixel_color = getpixelcolor.pixel(195, 229)
+    if pixel_color == (255, 181, 66):
+        battle()
+    else:
+        return False
+    
+def hp_check():
+    pass
+
+def shiny_check():
+    pass
+
+def stuck():
+    """
+    Attempts to check if user is stuck, and attempts to correct to allow grinding to continue.
+    """
+
+    #TODO: 
+    # Make modifiable within an ini file to allow user to choose whether to allow 
+    # stuck() function to attempt to correct or to just quit upon stuck detection.
+
+    # If no battle detected for ~1 min, attempt to unstuck...
+
+    pass
+
+def battle_menu_select():
+    pass
+
+def battle_move_menu_select():
+    pass
+
+def move_select():
+    """
+    Allows user to select which move to spam against wild pokemon.
+    """
+    #1,2,3,4...
+    pass
 
 def get_fg_window_title():
     """
-    Function to return title of currently active window.
+    Returns title of currently active window.
     """
 
     hWnd = ctypes.windll.user32.GetForegroundWindow()
@@ -18,11 +81,11 @@ def get_fg_window_title():
     
 def mgba_running_check():
     """
-    Function to check if mGBA is the active window.
+    Checks if mGBA is the active window.
     Returns True/False.
     """
 
-    # Countdown to allow user time to select mGBA as active window
+    # Countdown to allow user time to select mGBA as active window.
     print("Starting countdown for active window check!")
     print("Make sure mGBA is your active window now!")
     for i in range(5, 0, -1):
@@ -34,6 +97,8 @@ def mgba_running_check():
     title_check = title.find("mGBA")
     if title_check == 0:
         print("mGBA detected! Starting PokeGrinder...")
+        mgba_win_handle = win32gui.FindWindow(None, get_fg_window_title())
+        win32gui.MoveWindow(mgba_win_handle, 100, 100, 500, 400, 0) #TODO: Make x/y values modifiable within an ini file.
         time.sleep(1)
         return True
     else:
@@ -41,14 +106,10 @@ def mgba_running_check():
         time.sleep(3)
         return False
 
-def heal_pokemon_fr_ip():
-    pass
-
-def move_select():
-    #1,2,3,4...
-    pass
-
 def menu():
+    """
+    Menu function.
+    """
 
     print("Welcome to PokeGrinder! \nPlease ensure you have followed the instructions as stated in the README file.")
     start_loop = True
@@ -56,21 +117,32 @@ def menu():
         start = input("Please enter s to start PokeGrinder or q to quit. \n").upper()
         if start == "S":
             start_loop = False
+            mgba_check_menu_loop = True
+            while mgba_check_menu_loop:
+                mgba_check = mgba_running_check()
+                if mgba_check == True:
+                    mgba_check_menu_loop = False
+                else: 
+                    mgba_check_menu_loop = True
         elif start == "Q":
             exit()
+        elif start == "TEST": # Skips mGBA check sequence to allow for faster testing.
+            time.sleep(2.5)
+            start_loop = False
         else:
             print("Input not recognized.\n")
 
-    mgba_check_menu_loop = True
-    while mgba_check_menu_loop:
-        mgba_check = mgba_running_check()
-        if mgba_check == True:
-            mgba_check_menu_loop = False
-        else: 
-            mgba_check_menu_loop = True
+    #modules.frlg.fly_ip()
 
 def main():
-    menu()
+
+    #menu()
+    time.sleep(2)
+    igc.down()
+    igc.up()
+    igc.left()
+    igc.right()
 
 if __name__ == "__main__":
+
     main()
---
+++ modules/ingame_controls.py
@@ -0,0 +1,44 @@
+import time
+import keyboard
+
+def up():
+    time.sleep(0.2)
+    keyboard.press("up")
+    time.sleep(0.2)
+    keyboard.release("up")
+
+def down():
+    time.sleep(0.2)
+    keyboard.press("down")
+    time.sleep(0.2)
+    keyboard.release("down")
+
+def left():
+    time.sleep(0.2)
+    keyboard.press("left")
+    time.sleep(0.2)
+    keyboard.release("left")
+
+def right():
+    time.sleep(0.2)
+    keyboard.press("right")
+    time.sleep(0.2)
+    keyboard.release("right")
+
+def a():
+    pass
+
+def b():
+    pass
+
+def start():
+    pass
+
+def select():
+    pass
+
+def l_bumper():
+    pass
+
+def r_bumper():
+    pass
